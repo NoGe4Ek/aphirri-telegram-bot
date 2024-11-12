ff1dbf53d5959d7aa752dae28a87b80025434eab
Timofeev Kirill <kirill.timofeev.a@gmail.com>
2024-11-12 20:34:35 +0300

    add todo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,21 @@ import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 
 fun main() {
+    Runtime.getRuntime().availableProcessors()
     val bot = bot {
+
         token = "7721734270:AAGLddVTgVR4Htp3KmNjbJRfE6ehL5GBM9o"
         dispatch {
             text {
-                println(message.chat.id)
-                println(text)
 
-                bot.sendMessage(ChatId.fromId(message.chat.id), text = text)
-                message
+                // todo заменить эту проверку на какую-нибудь системную у бота
+                if (message.chat.id == -1002312275840) {
+                    println(message.from?.username)
+                    bot.sendMessage(ChatId.fromId(message.chat.id), text = text)
+                }
             }
         }
     }
+
     bot.startPolling()
 }
