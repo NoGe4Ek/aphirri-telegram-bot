aa6d24929c109079f2c4ecc65445b4e3f8535dae
Mitya Timofeev <gemdash222@gmail.com>
2024-11-17 19:55:01 +0300

    add command rate, storage system with map.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,28 @@
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 
 fun main() {
+    val clb = BaseInfo()
     Runtime.getRuntime().availableProcessors()
     val bot = bot {
 
         token = "7721734270:AAGLddVTgVR4Htp3KmNjbJRfE6ehL5GBM9o"
         dispatch {
             text {
+                clb.checkAndAdd(message.from?.id.toString(), message.from?.username.toString())
+                if (checkComm(text) == true) {
+                    // todo заменить эту проверку на какую-нибудь системную у бота
+                    clb.ScoreAdd(message.from?.id.toString(), text)
 
-                // todo заменить эту проверку на какую-нибудь системную у бота
-                if (message.chat.id == -1002312275840) {
-                    println(message.from?.username)
-                    bot.sendMessage(ChatId.fromId(message.chat.id), text = text)
                 }
+            }
+            command("rate") {
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = clb.RatingSet(message.from?.id.toString()))
             }
         }
     }
-
     bot.startPolling()
 }
